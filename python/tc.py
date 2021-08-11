8cb55a2434ed458a397b197611d1b77789c6ae49
Michel Pelletier <pelletier.michel@gmail.com>
2021-08-11 09:46:42 -0700

    added optional command line args to tc.py to run specific graphs. also readded PR

--- python/tc.py
+++ python/tc.py
@@ -1,4 +1,4 @@
-from timeit import repeat
+import timeit
 from collections import defaultdict
 from pygraphblas import *
 from pygraphblas.descriptor import T0, ST0, ST1
@@ -33,22 +33,14 @@ def TC1(A):
     return (3 * (A @ y) - 2 * (T.one() @ y) + y) / k
 
 
-def TC2(A):
-    T = A.plus_pair(A, mask=A, desc=ST1)
-    y = Vector.dense(FP64, A.nrows)
-    T.reduce_vector(out=y, accum=FP64.plus)
-    k = y.reduce_float()
-    return (3 * A.plus_second(y) - 2 * T.plus_second(y) + y) / k
-
-
 def TC3(A):
     M = A.tril(-1)
     T = A.plus_pair(A, mask=M, desc=ST1)
     y = T.reduce() + T.reduce(desc=ST0)
     k = y.reduce_float()
-    return (
-        3 * A.plus_second(y) - (2 * (T.plus_second(y) + T.plus_second(y, desc=ST0))) + y
-    ) / k
+    T2 = T.plus_second(y) + T.plus_second(y, desc=ST0)
+    r = (3 * A.plus_second(y)) - (2 * T2) + y
+    return r / k
 
 
 def tcount(A):
@@ -56,47 +48,53 @@ def tcount(A):
     return L.plus_pair(L, mask=L).reduce_int()
 
 
-graphs = [
-     "Newman/karate",
-     "SNAP/com-Youtube",
-     "SNAP/as-Skitter",
-     "SNAP/com-LiveJournal",
-     'SNAP/com-Orkut',
-     'SNAP/com-Friendster'
-]
-
-
 @binary_op(FP64)
 def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
     return abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
 
-R = 3
-for name in graphs:
-    print(f"Loading {name}")
-    G = dict(Matrix.ssget(name, binary_cache_dir="~/.ssgetpy"))[
-        name.split("/")[1] + ".mtx"
-    ]
-    G = G.cast(FP64)
-    G.wait()
-    results = defaultdict(dict)
-    print(f"{name} | {G.shape} | {G.nvals} edges | {tcount(G)} triangles")
-    # for centrality in TC1, TC2, TC3:
-    for centrality in TC1, TC3:
-        fname = centrality.__name__
-        print(f"Running {fname} on {name} {R} times")
-        result = repeat(
-            "results[name][fname] = centrality(G)", repeat=R, number=1, globals=locals()
-        )
-        print(
-            f"{fname} on {name} took ",
-            sum(result) / len(result),
-            f"average for {R} runs",
-        )
-    tc1 = results[name]["TC1"].nonzero()
-    # tc2 = results[name]["TC2"].nonzero()
-    tc3 = results[name]["TC3"].nonzero()
-    # print(f"TC1 equal to TC2? {tc1.iseq(tc2, isclose)}")
-    # print(f"TC2 equal to TC3? {tc2.iseq(tc3, isclose)}")
-    print(f"TC1 equal to TC3? {tc1.iseq(tc3, isclose)}")
-    # breakpoint()
+def main(graphs, repeat=3):
+    for name in graphs:
+        print(f"Loading {name}")
+        G = dict(Matrix.ssget(name, binary_cache_dir="~/.ssgetpy"))[
+            name.split("/")[1] + ".mtx"
+        ]
+        G = G.cast(FP64)
+        G.wait()
+        results = defaultdict(dict)
+        print(f"{name} | {G.shape} | {G.nvals} edges | {tcount(G)} triangles")
+        for centrality in PR, TC1, TC3:
+            fname = centrality.__name__
+            print(f"Running {fname} on {name} {repeat} times")
+            result = timeit.repeat(
+                "results[name][fname] = centrality(G)",
+                repeat=repeat,
+                number=1,
+                globals=locals(),
+            )
+            print(
+                f"{fname} on {name} took ",
+                sum(result) / len(result),
+                f"average for {repeat} runs",
+            )
+        tc1 = results[name]["TC1"].nonzero()
+        tc3 = results[name]["TC3"].nonzero()
+        print(f"TC1 equal to TC3? {tc1.iseq(tc3, isclose)}")
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 1:
+        graphs = [
+            "Newman/karate",
+            "SNAP/com-Youtube",
+            "SNAP/as-Skitter",
+            "SNAP/com-LiveJournal",
+            "SNAP/com-Orkut",
+            "SNAP/com-Friendster",
+        ]
+    else:
+        graphs = sys.argv[1:]
+
+    main(graphs)
