bd7a896987fe53758cdefb23197b2af21f6ac2ab
Wimmerer <wrkimmerer@outlook.com>
2021-08-16 15:11:23 -0400

    fixes again

--- julia/script.jl
+++ julia/script.jl
@@ -1,7 +1,7 @@
 using SuiteSparseMatrixCollection
 using MatrixMarket
 using SuiteSparseGraphBLAS
-gbset(SuiteSparseGraphBLAS.FORMAT, SuiteSparseGraphBLAS.BYROW)
+SuiteSparseGraphBLAS.gbset(SuiteSparseGraphBLAS.FORMAT, SuiteSparseGraphBLAS.BYROW)
 using BenchmarkTools
 using SparseArrays
 include("tc.jl")
@@ -20,14 +20,14 @@ matrices = filter(row -> row.name ∈ graphs, ssmc)
 BenchmarkTools.DEFAULT_PARAMETERS.gcsample = true
 for name ∈ graphs
     path = fetch_ssmc(matrices[matrices.name .== name, :])[1]
-    G = GBMatrix(sortSparseMatrixCSC!(convert(SparseMatrixCSC{Float64}, MatrixMarket.mmread(joinpath(path, "$name.mtx")))))
+    G = GBMatrix(SparseArrays.sortSparseMatrixCSC!(convert(SparseMatrixCSC{Float64}, MatrixMarket.mmread(joinpath(path, "$name.mtx")))))
     SuiteSparseGraphBLAS.gbset(G, SuiteSparseGraphBLAS.FORMAT, SuiteSparseGraphBLAS.BYROW)
     show(stdout, MIME"text/plain"(), G)
     GC.gc()
     println("$name | $(size(G)) | $(nnz(G)) edges")
     for centrality in [PR, TC1, TC3]
         println("Benchmarking $(string(centrality)) on $(name)")
-        result = @benchmark $centrality($G) samples=3 seconds=600
+        result = @benchmark $centrality($G) samples=3 seconds=100
         show(stdout,MIME"text/plain"(),result)
     end
 end
