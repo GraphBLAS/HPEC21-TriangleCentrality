365926fb8c13578a256fd028e16228fb375bbf7e
Wimmerer <wrkimmerer@outlook.com>
2021-08-17 19:58:26 -0400

    Fix some degree issues

--- julia/pr.jl
+++ julia/pr.jl
@@ -2,8 +2,9 @@ function PR(A, d, α = 0.85, maxiters = 100, ϵ = 1.0e-4)
     n = size(A, 1)
     r = GBVector(n, 1.0 / n)
     t = GBVector{Float64}(n)
-    d = copy(d)
-    d[:, accum=/] = α
+    d = d ./ α
+    dmin = GBVector(n, 1.0 / n)
+    eadd!(d, d, dmin, max)
     teleport = (1 - α) / n
     for _ ∈ 1:maxiters
         temp = t; t = r; r = temp
