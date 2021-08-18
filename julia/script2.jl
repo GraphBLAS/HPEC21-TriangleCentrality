aae13f728d7b302645ec0bf65be1b23d6213be7a
Tim Davis <davis@tamu.edu>
2021-08-18 13:06:31 -0500

    more julia results

---
+++ julia/script2.jl
@@ -0,0 +1,41 @@
+using SuiteSparseMatrixCollection
+using MatrixMarket
+using SuiteSparseGraphBLAS
+SuiteSparseGraphBLAS.gbset(SuiteSparseGraphBLAS.FORMAT, SuiteSparseGraphBLAS.BYROW)
+using BenchmarkTools
+using SparseArrays
+using LinearAlgebra
+include("tc.jl")
+include("pr.jl")
+graphs = [
+    #"karate",
+    #"com-Youtube",
+    #"as-Skitter",
+    #"com-LiveJournal",
+    #"com-Orkut",
+    "com-Friendster",
+]
+
+ssmc = ssmc_db()
+matrices = filter(row -> row.name ∈ graphs, ssmc)
+BenchmarkTools.DEFAULT_PARAMETERS.gcsample = true
+for name ∈ graphs
+    path = fetch_ssmc(matrices[matrices.name .== name, :])[1]
+    G = GBMatrix(convert(SparseMatrixCSC{Float64}, MatrixMarket.mmread(joinpath(path, "$name.mtx"))))
+    SuiteSparseGraphBLAS.gbset(G, SuiteSparseGraphBLAS.FORMAT, SuiteSparseGraphBLAS.BYROW)
+    GC.gc()
+    G[:,:, mask=G, desc=SuiteSparseGraphBLAS.S] = 1
+    diag(G)
+    println("$name | $(size(G)) | $(nnz(G)) edges")
+    d = reduce(+, G; dims=2)
+    # for centrality in [PR, TC1, TC3]
+    for centrality in [TC3]
+        println("Benchmarking $(string(centrality)) on $(name)")
+        i = 0.0
+        centrality(G, d) #warmup
+        for run ∈ 1:3
+            i += @elapsed centrality(G, d)
+        end
+        println("$(string(centrality)) on $(name) over 3 runs took an average of: $(i / 3)s")
+    end
+end
---
+++ julia/script3.jl
@@ -0,0 +1,44 @@
+using SuiteSparseMatrixCollection
+using MatrixMarket
+using SuiteSparseGraphBLAS
+SuiteSparseGraphBLAS.gbset(SuiteSparseGraphBLAS.FORMAT, SuiteSparseGraphBLAS.BYROW)
+using BenchmarkTools
+using SparseArrays
+using LinearAlgebra
+include("tc.jl")
+include("pr.jl")
+graphs = [
+    #"karate",
+    "com-Youtube",
+    #"as-Skitter",
+    #"com-LiveJournal",
+    #"com-Orkut",
+    "com-Friendster",
+]
+
+ssmc = ssmc_db()
+matrices = filter(row -> row.name ∈ graphs, ssmc)
+BenchmarkTools.DEFAULT_PARAMETERS.gcsample = true
+for name ∈ graphs
+    path = fetch_ssmc(matrices[matrices.name .== name, :])[1]
+    G = GBMatrix(convert(SparseMatrixCSC{Float64}, MatrixMarket.mmread(joinpath(path, "$name.mtx"))))
+    SuiteSparseGraphBLAS.gbset(G, SuiteSparseGraphBLAS.FORMAT, SuiteSparseGraphBLAS.BYROW)
+    GC.gc()
+    G[:,:, mask=G, desc=SuiteSparseGraphBLAS.S] = 1
+    diag(G)
+    println("$name | $(size(G)) | $(nnz(G)) edges")
+    d = reduce(+, G; dims=2)
+    # for centrality in [PR, TC1, TC3]
+    for centrality in [TC3]
+        println("Benchmarking $(string(centrality)) on $(name)")
+        i = 0.0
+        j = @elapsed centrality(G, d) #warmup
+        println("warmup $(j)")
+        for run ∈ 1:3
+            j = @elapsed centrality(G, d)
+            println("trial time: $(j)")
+            i += j
+        end
+        println("$(string(centrality)) on $(name) over 3 runs took an average of: $(i / 3)s")
+    end
+end
